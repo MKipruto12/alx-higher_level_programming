24cda45fd54eb2d9611422cf3fe4b1a0cb5c0852
MKipruto12 <mercykipruto2017@gmail.com>
2022-06-06 15:00:59 -0700

    pep-8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,6 +8,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
-    total_tuple = (int(tuple_a[0]) + int(tuple_b[0]),
+    final_tuple = (int(tuple_a[0]) + int(tuple_b[0]),
                  int(tuple_a[1]) + int(tuple_b[1]))
-    return total_tuple
+    return final_tuple
