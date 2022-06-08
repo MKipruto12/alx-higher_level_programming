d1ed7a82f28bcd4c9af9bdff3e54cda0dc38af9e
MKipruto12 <mercykipruto2017@gmail.com>
2022-06-08 12:28:12 -0700

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import *
     import sys
 
-    if len(sys.argv[1:]) != 3:
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operators = ["+", "-", "*", "/"]
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if sys.argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
-        if sys.argv[2] == operators[0]:
-            result = add(a, b)
-        elif sys.argv[2] == operators[1]:
-            result = sub(a, b)
-        elif sys.arg[2] == operators[2]:
-            result = mul(a, b)
-        elif sys.argv[2] == operators[3]:
-            result = div(a, b)
-
-    print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], result))
+        print("{} / {} = {}".format(a, b, div(a, b)))
