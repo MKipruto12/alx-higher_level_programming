3a1c1ff39f314f38ceb3703693f29b5585318329
MKipruto12 <mercykipruto2017@gmail.com>
2022-06-22 14:52:41 -0700

    modified

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div_result = a / b
+        result = a / b
     except:
-        div_result = None
+        result = None
     finally:
-        print("inside result: {}".format(div_result))
-    return (div_result)
+        print("inside result: {}".format(result))
+    return (result)
